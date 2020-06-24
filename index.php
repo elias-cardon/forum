a184d1818c2e10026d2697e1e7c2f2d7101e38d4
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-24 14:39:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!-- Negotium est negotium -->
 <?php session_start();
 
 if (isset($_POST["deconnexion"])) {
@@ -30,53 +29,56 @@ if (isset($_POST["deconnexion"])) {
 
         <a href="topic.php">Insérer un sujet</a>
 
-<br /><br />
+        <br/><br/>
 
-<?php
-$bdd = mysqli_connect('localhost', 'root', '');
-mysqli_select_db($bdd, 'forum');
+        <?php
+        $bdd = mysqli_connect('localhost', 'root', '');
+        mysqli_select_db($bdd, 'forum');
 
-$sql = 'SELECT id, titre, date_heure FROM topics ORDER BY date_heure DESC';
+        $sql = 'SELECT id, titre, date_heure FROM topics ORDER BY date_heure DESC';
 
-$req = mysqli_query($bdd, $sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error());
+        $req = mysqli_query($bdd, $sql) or die('Erreur SQL !<br />' . $sql . '<br />' . mysqli_error());
 
-$nb_sujets = mysqli_num_rows ($req);
+        $nb_sujets = mysqli_num_rows($req);
 
-if ($nb_sujets == 0) {
-    echo 'Aucun sujet';
-}
-else {
-?>
-<table width="500" border="1"><tr>
-    <td>
-    Auteur
-    </td><td>
-    Titre du sujet
-    </td><td>
-    Date dernière réponse
-    </td></tr>
-    <?php
-    while ($data = mysql_fetch_array($req)) {
+        if ($nb_sujets == 0) {
+            echo 'Aucun sujet';
+        } else {
+            ?>
+            <table width="500" border="1">
+                <tr>
+                    <td>
+                        Auteur
+                    </td>
+                    <td>
+                        Titre du sujet
+                    </td>
+                    <td>
+                        Date dernière réponse
+                    </td>
+                </tr>
+                <?php
+                while ($data = mysql_fetch_array($req)) {
 
-        sscanf($data['date_derniere_reponse'], "%4s-%2s-%2s %2s:%2s:%2s", $annee, $mois, $jour, $heure, $minute, $seconde);
+                    sscanf($data['date_derniere_reponse'], "%4s-%2s-%2s %2s:%2s:%2s", $annee, $mois, $jour, $heure, $minute, $seconde);
 
-        echo '<tr>';
-    echo '<td>';
+                    echo '<tr>';
+                    echo '<td>';
 
-    echo htmlentities(trim($data['auteur']));
-    echo '</td><td>';
+                    echo htmlentities(trim($data['auteur']));
+                    echo '</td><td>';
 
-    echo '<a href="./lire_sujet.php?id_sujet_a_lire=' , $data['id'] , '">' , htmlentities(trim($data['titre'])) , '</a>';
+                    echo '<a href="./lire_sujet.php?id_sujet_a_lire=', $data['id'], '">', htmlentities(trim($data['titre'])), '</a>';
 
-    echo '</td><td>';
-echo $jour , '-' , $mois , '-' , $annee , ' ' , $heure , ':' , $minute;
-    }
-    ?>
-    </td></tr></table>
-    <?php
-}
-mysqli_free_result ($req);
-?>
+                    echo '</td><td>';
+                    echo $jour, '-', $mois, '-', $annee, ' ', $heure, ':', $minute;
+                }
+                ?>
+                </td></tr></table>
+            <?php
+        }
+        mysqli_free_result($req);
+        ?>
     </div>
 
 </main>
