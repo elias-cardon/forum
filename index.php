0fcfcf6653807bac22907cfe3636aa091d5e8076
EliasCardon <62133614+EliasCardon@users.noreply.github.com>
2020-06-24 14:26:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -27,6 +27,56 @@ if (isset($_POST["deconnexion"])) {
 <main>
     <div id="banner">
         <h2>BIENVENUE</h2>
+
+        <a href="./insert_sujet.php">Insérer un sujet</a>
+
+<br /><br />
+
+<?php
+$bdd = mysqli_connect('localhost', 'root', '');
+mysqli_select_db($bdd, 'forum');
+
+$sql = 'SELECT id, titre, date_heure FROM topics ORDER BY date_heure DESC';
+
+$req = mysqli_query($bdd, $sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysqli_error());
+
+$nb_sujets = mysqli_num_rows ($req);
+
+if ($nb_sujets == 0) {
+    echo 'Aucun sujet';
+}
+else {
+?>
+<table width="500" border="1"><tr>
+    <td>
+    Auteur
+    </td><td>
+    Titre du sujet
+    </td><td>
+    Date dernière réponse
+    </td></tr>
+    <?php
+    while ($data = mysql_fetch_array($req)) {
+
+        sscanf($data['date_derniere_reponse'], "%4s-%2s-%2s %2s:%2s:%2s", $annee, $mois, $jour, $heure, $minute, $seconde);
+
+        echo '<tr>';
+    echo '<td>';
+
+    echo htmlentities(trim($data['auteur']));
+    echo '</td><td>';
+
+    echo '<a href="./lire_sujet.php?id_sujet_a_lire=' , $data['id'] , '">' , htmlentities(trim($data['titre'])) , '</a>';
+
+    echo '</td><td>';
+echo $jour , '-' , $mois , '-' , $annee , ' ' , $heure , ':' , $minute;
+    }
+    ?>
+    </td></tr></table>
+    <?php
+}
+mysqli_free_result ($req);
+?>
     </div>
 
 </main>
