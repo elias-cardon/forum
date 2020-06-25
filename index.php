ccdfdee6fabd9a8ef801697b42af2189af14c11c
Bryan Gil <bryan.gil@laplateforme.io>
2020-06-25 14:26:51 +0200

    jsp

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ if (isset($_POST["deconnexion"])) {
                     echo htmlentities(trim($data['login']));
                     echo '</td><td>';
 
-                    echo '<a href="topic.php', $data['id'], '">', htmlentities(trim($data['titre'])), '</a>';
+                    echo '<a href="#', $data['id'], '">', htmlentities(trim($data['titre'])), '</a>';
 
                     echo '</td><td>';
                     echo $jour, '-', $mois, '-', $annee, ' ', $heure, ':', $minute;
