a2821a9022612f1be1b179cc1156d000944984fa
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2024-02-25 22:40:57 +0100

    On recommence le projet

--- include/footer.php
+++
@@ -1,4 +0,0 @@
-
-        <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
-        <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
-        <a href="https://www.instagram.com/el_eliohs/" target="_blank"><i class="fab fa-instagram"></i></a>
